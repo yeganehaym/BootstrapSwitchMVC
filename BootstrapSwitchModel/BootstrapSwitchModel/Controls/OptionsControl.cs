25780014ce21dded8ba1f476768b91ff60c042e7
yeganehaym <im3xt6mqt>
2015-07-10 19:26:50 +0430

    create BootstrapSwitch control so easy, please import bootstrapswitch style and script file in your project

---
+++ BootstrapSwitchModel/BootstrapSwitchModel/Controls/OptionsControl.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace BootstrapSwitchModel.Controls
+{
+    public static class OptionsControl
+    {
+        public static MvcHtmlString BootstrapSwitch(this HtmlHelper helper, string controlName, Models.BootstrapSwitchModel model, bool defineScript = true)
+        {
+            string code = "<input type='checkbox' id='{0}' name='{0}'  {1} /> ";
+
+            code = string.Format( code, controlName, GetBootstrapSwitchModelProperties(model));
+
+            if (defineScript)
+            {
+                string script = "<script>$(document).ready(function () {{$('#{0}').bootstrapSwitch();}});</script>";
+                script = string.Format(script, controlName);
+                code = script + code;
+            }
+
+            return MvcHtmlString.Create(code);
+        }
+
+        private static string GetBootstrapSwitchModelProperties(Models.BootstrapSwitchModel model)
+        {
+            string stringProperties = "";
+
+            var properties = model.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                foreach (var attribute in property.GetCustomAttributes(true))
+                {
+                    var displayAttribute = attribute as DisplayNameAttribute;
+
+                    if (displayAttribute != null)
+                    {
+                        string name = displayAttribute.DisplayName;
+                        string oneproperty = " " + name + "='{0}' ";
+                        string value = "";
+                        object objvalue = property.GetValue(model);
+                        if (CheckCondition(objvalue))
+                        {
+                            value = objvalue.ToString().ToLower();
+                        stringProperties += String.Format(oneproperty, value);
+                        }
+                    break;
+                    } 
+                }
+            }
+
+            return stringProperties;
+        }
+
+        private static bool CheckCondition(object objvalue)
+        {
+            if (objvalue == null) return false;
+
+            object boolobj = false;
+            if (objvalue.Equals(boolobj)) return false;
+
+            if (string.IsNullOrWhiteSpace(objvalue.ToString())) return false;
+            if (objvalue.ToString().ToLower() == "default") return false;
+
+            return true;
+        }
+    }
+}
---
+++ BootstrapSwitchModel/BootstrapSwitchModel/Models/BootstrapSwitchModel.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+
+namespace BootstrapSwitchModel.Models
+{
+    public class BootstrapSwitchModel
+    {
+        [DisplayName("data-size")]
+        public BootstrapSwitchSize Size { get; set; }
+
+        [DisplayName("checked")]
+        public bool Checked { get; set; }
+
+        [DisplayName("data-animate")]
+        public bool Animate { get; set; }
+
+        [DisplayName("readonly")]
+        public bool ReadOnly { get; set; }
+
+        [DisplayName("data-on-color")]
+        public BootstrapColor OnColor { get; set; }
+
+        [DisplayName("data-off-color")]
+        public BootstrapColor OffColor { get; set; }
+
+        [DisplayName("data-on-text")]
+        public string OnText { get; set; }
+
+        [DisplayName("data-off-text")]
+        public string OffText { get; set; }
+
+        [DisplayName("data-label-text")]
+        public string LabelText { get; set; }
+
+        [DisplayName("data-label-width")]
+        public int LabelWidth { get; set; }
+
+
+        [DisplayName("data-base-class")]
+        public string BaseClass { get; set; }
+
+        [DisplayName("data-wrapper-class")]
+        public string WrapperClass { get; set; }
+
+        [DisplayName("data-radio-all-off")]
+        public bool RadioAllOff { get; set; }
+
+        [DisplayName("inverse")]
+        public bool Inverse { get; set; }
+
+        [DisplayName("data-indeterminate")]
+        public bool Indeterminate { get; set; }
+
+        [DisplayName("disabled")]
+        public bool Disabled { get; set; }
+
+        [DisplayName("data-handle-width")]
+        public bool HandleWidth { get; set; }
+    }
+
+    public enum BootstrapSwitchSize
+    {
+        Mini=0,
+        Small=1,
+        Normal=2,
+        Large=3
+    }
+
+    public enum BootstrapColor
+    {
+        Default,
+        Primary,
+        Info,
+        Success,
+        Danger,
+        Warning   
+    }
+
+
+}
